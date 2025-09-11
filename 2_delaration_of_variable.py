3706f3719eb7f493968c538d9e8d86891cae360b
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-11 17:22:25 +0530

    Create 2_delaration_of_variable.py

---
+++ 2_delaration_of_variable.py
@@ -0,0 +1,15 @@
+# comments are something ignored by python interpreter 
+
+# variables in python 
+# you cannot use numbers at starting
+# cannot you spaces
+# cannot use (_ )special charecters as variable
+# 1. PascalCase 
+# 2. camelCase
+# 3. snake_case
+
+sher = "lion"
+print(sher)
+
+
+print('hello again doing python');
