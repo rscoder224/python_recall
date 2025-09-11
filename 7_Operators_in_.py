abb162a5f99a796ace5d0c97dad6b4dc90da0314
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-11 21:58:43 +0530

    Create 7_Operators_in_.py

---
+++ 7_Operators_in_.py
@@ -0,0 +1,54 @@
+# operators in python
+
+# Arthematic Operators in Python
+# (+,-,*,/,%,//,**) 7
+
+a = 12
+
+b= 10
+
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+print(a%b)
+# it will follow bodmas
+#  floor division convert this into integer
+print(a//b)
+print(a**b)
+
+#  Assingment Operator
+# =
+# +=
+# -=
+# *=
+# /=
+# %=
+# //=
+# **=
+# compound Assignment operations
+
+c = 0
+c += 20
+c += 40
+c += 60
+
+c//=2
+
+print(c)
+
+# comparison Operators
+# comparison operators always provide boolean value i.e True or False
+#  ==
+# 
+
+
+
+
+
+
+
