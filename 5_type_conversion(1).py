1ab266aa90a3477275ae7476c632adc55f848a16
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-11 21:29:30 +0530

    Create 5_type_conversion(1).py

---
+++ 5_type_conversion(1).py
@@ -0,0 +1,53 @@
+# Type conversion 
+# implicit and Explicit Type conversion
+# type conversion refers to the conversion of one data type into another
+
+# explicit type coversion
+# int ==> str
+
+a = 12
+
+print(type(a))
+print(a)
+
+a = str(a)
+
+print(type(a))
+
+# str ==> int
+
+b = '14'
+
+b = int(b)
+
+print(b)
+print(type(b))
+# truthy and 7 falsy value False 0 ,0.0 ,"", emptylist[], emptytouple()
+# empty {} dictonery
+
+print(bool(""))
+
+# implicit type consversion
+
+n = 12/4
+
+print(n)
+print(type(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
