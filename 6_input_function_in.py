b7b31fbcdb0f263e6a479023c142bff40acc4517
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-11 21:43:16 +0530

    Create 6_input_function_in.py

---
+++ 6_input_function_in.py
@@ -0,0 +1,18 @@
+# input and output
+
+name = str(input("Enter Your Name : "))
+
+age = int(input("Enter your age : "))
+
+happy_birthday = f"{name} birthday to you now you are {age}"
+
+print(happy_birthday)
+
+first_name = str(input("Enter your Firstname Here"))
+
+lastName = str(input("Enter your Lastname Here"))
+
+fullName = f" your fullname is {first_name} {lastName}"
+
+print(fullName)
+
