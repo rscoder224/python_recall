c86b09acce7ab5945ffa4c9d50c862ce6db053b0
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-11 17:34:54 +0530

    Create 3_datatype_in.py

---
+++ 3_datatype_in.py
@@ -0,0 +1,73 @@
+# we you variables to store data types
+# numbers, string and booleans
+
+
+# int datatype
+
+a = 12
+print(a)
+print(type(a))
+
+
+# float datatype
+
+b = 2.2
+
+print(b)
+print(type(b))
+
+# complex datatype
+
+c = 23j
+
+print(c);
+print(type(c));
+
+
+# string datatype
+# combination of words
+
+name = 'Shaurya'
+
+print(name)
+print(type(name))
+# getting unicode of sting
+# ord
+# char to unicode 
+print(ord(name[0]))
+# unicode to charcter
+print(chr(83))
+
+# boolean
+
+x = True
+
+print(type(x))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
