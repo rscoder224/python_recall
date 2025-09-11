0faee72edf540798087dd17b9749b61f028952ea
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-11 20:59:05 +0530

    Create 4_string_indexing&slicing.py

---
+++ 4_string_indexing.py
@@ -0,0 +1,34 @@
+
+# string indexing
+
+name = 'shaurya tiwari'
+
+print(name[0])
+
+print(name[-7])
+# string Slicing
+
+sirname = name[8:14]
+print(sirname)
+# syntax for slicing
+# [start_index,end_index,steps_of_slicing]
+
+firstName = name[0:7:1]
+print(firstName)
+# lastName = name[8::]  ===> lastName name[8::1] both work same
+lastName = name[8::]
+
+print(lastName)
+
+
+
+
+
+
+
+
+
+
+
+
+
