bb064cf0dc1381520f7e7e759797227040719f17
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-12 23:09:00 +0530

    Create 10_for_loop.py

---
+++ 10_for_loop.py
@@ -0,0 +1,70 @@
+# for loop is used for number where While loop works on the basis of condition
+
+# for loop
+# range accepts three things same as slicing Start stop and steps range()
+
+# 
+
+# a = range(1,21,1)
+
+# for i in a:
+#     print("hello I am here")
+    
+    
+    
+
+# b = range(21)
+
+# for i in b:
+#     print("hello I am here")
+    
+
+# c = range(20,51,1)
+
+# for i in c:
+#     print(i)
+    
+# d = range(16,0,-1)
+
+# for i in d:
+#     print(i)
+    
+
+# e = range(-3,-16,-1)
+
+# for i in e:
+#     print(i)
+    
+# tab_five = range(1,11,1)
+
+# for i in tab_five:
+#     print(f"5 X {i} = {5*i}")
+    
+
+# tab_six = range(6,61,6)
+
+# for i in tab_six:
+#     print(i)
+    
+
+n = int(input("Enter A number to print a table bro==> "))
+
+for i in range(n,(n*10)+1,n):
+    print(i)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
