fbb5517c16c9cdcc3f344bce06666058d628317c
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-12 17:03:06 +0530

    Update 7_Operators_in_.py

--- 7_Operators_in_.py
+++ 7_Operators_in_.py
@@ -1,3 +1,5 @@
+
+
 # operators in python
 
 # Arthematic Operators in Python
@@ -43,8 +45,36 @@ print(c)
 
 # comparison Operators
 # comparison operators always provide boolean value i.e True or False
-#  ==
-# 
+#  == equals
+#  != not equal
+#  >  greater than
+#  <  less Than
+#  >= greater than equal to
+#  <= less than equal to
+
+
+print(c==a)
+print(a!=b)
+print(a>b)
+print(c<b)
+print(b<=c)
+print(c>=b)
+
+print('logical operators')
+# logical operators
+
+print(a == b and c==b)
+
+print(a>b or b<a)
+
+
+print(not 6==6)
+
+
+
+
+
+
 
 
 
