5150c715b14492c52746de9539129512abbf7261
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-12 20:53:36 +0530

    Create 8_if_elif_else.py

---
+++ 8_if_elif_else.py
@@ -0,0 +1,41 @@
+
+# age = 12
+
+# if age >= 18:
+#     print('Can Cast Vote')
+# else: 
+#     print("Can't cast vote ")
+    
+# drink = 14
+# if drink >= 18:
+#     print("Amazing Drink")
+# elif drink > 12:
+#     print("drink is selected")
+# else:
+#     print("Your drink is not selected")
+                
+               
+#   age = 13
+   
+# age = 19
+# if age > 19:
+#     print('yes')
+# elif age >15:
+#     print("Yes But K")
+# else:
+#     print("No Never")
+    
+age = int(input("Enter your age: "))
+
+if age >= 55:
+    print("You are a senior cetizen can cast the vote")
+elif age >= 18:
+    print("You can cast the vote")
+else:
+    print("can't cast the vote")
+    
+    
+    
+    
+    
+    
