f0766a0f3dbce257ef500227f1a2de0926060af0
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-12 21:39:55 +0530

    Create 9_python_questions_.py

---
+++ 9_python_questions_.py
@@ -0,0 +1,82 @@
+# Q1 Accept two numbers and print greatest b/w them
+# a = int(input("enter a number a: "))
+
+# b = int(input("enter a number b: "))
+
+
+# if a > b:
+#   print("A is the greatest")
+
+# elif b > a:
+#   print("B is the gretest")
+
+# else:
+#   print("Both are equal")
+  
+# Q2 Accept the gender from the user as char and print the respective greeting
+
+# gender = str(input("Enter you Gender: "))
+
+# if gender == "male":
+#     print("You are a boy or Man")
+
+# elif gender == "female":
+#     print("You are a girl or Women")
+# else:
+#     print("You are Unique")
+
+# Q3 Accept an integer and check whether it is even or odd
+
+# integer = int(input("Enter a number: "))
+
+# if integer%2==0:
+#     print("It's Even")
+# else:
+#     print("its odd")
+
+
+# Q4 Accept name and age from user and check valid voter or not
+
+# voter_name = str(input("Enter your name here: "))
+
+# voter_age = int(input("Enter your age here: "))
+
+# if voter_age >= 18:
+#     print(f"{voter_name} You can cast vote")
+# else:
+#     print(f"{voter_name} you can't cast becuase under age")
+
+# Q5 Accept a year and check it is a leap year or not
+
+# year = int(input("Enter Year Over here: "))
+
+# if year%4==0 and year%100 != 0:
+#     print("This is a Leap year")
+# else: 
+#     print("This is not a Leap year")
+    
+
+temp = int(input("Enter Tempreture Here: "))
+
+if temp > 40:
+    print("Very Hot")
+elif temp <= 40 and temp >=30:
+    print("Hot")
+elif temp <= 30 and temp >= 20:
+    print("Pleasent")
+elif temp <=20 and temp >10:
+    print('cold')
+elif temp <= 10 and temp >0:
+    print("Very Cold")
+else:
+    print("Freezing cold")
+    
+    
+    
+    
+    
+    
+
+
+  
+
