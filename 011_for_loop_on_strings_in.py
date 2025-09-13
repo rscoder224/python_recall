efdab7301b1037a039f1307e8ffabaa0dd5031e6
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-13 17:26:07 +0530

    Create 011_for_loop_on_strings_in.py

---
+++ 011_for_loop_on_strings_in.py
@@ -0,0 +1,16 @@
+
+# for loop on string
+
+# print(len("Shaurya"))
+
+
+# a = str(input("enter your name: "))
+# print(f" length is  {len(a)}")
+# for i in range(0,len(a),1):
+#     print(a[i])
+    
+# for i in a:
+#   print(i)
+    
+    
+    
