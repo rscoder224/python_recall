84946137c7114ed1424a5f407c683480655b90de
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-15 21:55:55 +0530

    Create 012_break&continue_keyword.py

---
+++ 012_break&continue_keyword.py
@@ -0,0 +1,15 @@
+# break continue and else statement
+# for i in range(1,26,1):
+#   if i == 15:
+#     break 
+#   else:
+#     print(i)
+    
+    
+for i in range(1,26,1):
+    if i == 21:
+        continue
+    else:
+        print(i)
+        
+        
