9ee5b1a2960cfa9e0818440969a842922c666b95
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-16 22:11:08 +0530

    Create 15_practice.py

---
+++ 15_practice.py
@@ -0,0 +1,15 @@
+# reverse a loop print n to 1
+
+# a = int(input("Enter a Number: "))
+
+# for i in range(a,0,-1):
+#     print(i)
+    
+# c = int(input("Enter the table which you want to print: "))
+# a = range(1,11,1)
+    
+    
+# for i in a:
+#     print(f"{c} X {i} = {i*c}")
+    
+    
