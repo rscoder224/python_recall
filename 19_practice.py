d64fdfc16fb2ca48b4a8a23825faffa4f5c3af1b
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-17 20:30:13 +0530

    Create 19_practice.py

---
+++ 19_practice.py
@@ -0,0 +1,50 @@
+
+# factors
+
+# Perfect Number
+
+
+# n = int(input("enter a number :  "))
+# sum = 0
+# for i in range(1,n,1):
+#         if n%i==0:
+#           sum = sum + i
+#           print(sum)
+            
+            
+
+# if sum == n:
+#     print("its a perfect number")
+# else:
+#     print("its not a perfect number")
+    
+    
+a = "6237454gfg5c&%^%%*("
+
+char = 0
+dig = 0
+special = 0
+
+for i in a:
+    if i.isdigit():
+        dig += 1
+    elif i.isalpha():
+        char += 1
+    else:
+        special += 1
+        
+print(f"special charecter:==> {special} digits are ==> {dig} charecter are ==> {char}")
+
+
+
+
+    
+         
+             
+
+ 
+
+ 
+ 
+       
+       
